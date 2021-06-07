e0b1682c6a8c519d24fa72ae40d32770163fe371
kmth-hash <82401957+kmth-hash@users.noreply.github.com>
2021-06-07 20:25:30 +0530

    Update settings.py

--- myproj/myproj/settings.py
+++ myproj/myproj/settings.py
@@ -66,7 +66,7 @@ DATABASES = {
     'ENGINE': 'django.db.backends.postgresql_psycopg2',
     'NAME': 'I2R_DB',
     'USER': 'postgres',
-    'PASSWORD': 'dimi1234',
+    'PASSWORD': 'postgres',
     'HOST': 'localhost',
     'PORT': '',
     }
