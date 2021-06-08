674ac312808d7102e4a3eedc941dc37d343186ad
vinayak <vinayakudupa@gmail.com>
2021-06-08 09:12:43 +0530

    vin

--- myproj/myproj/views.py
+++ myproj/myproj/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+from json import dumps,loads
 
-
-
-
+imagesToPreview=[]
+datajson=[]
 def signup(request):
     return render(request , 'signup.html' , {})
 
@@ -10,7 +11,17 @@ def login(request):
     return render(request , 'login.html' , {})
 
 def mainpage(request):
-    return render(request , 'mainpage.html' , {})
+    global imagesToPreview,datajson
+    if request.POST:
+        imagesToPreview = loads(request.POST['hiddeninput'])
+        doc = request.FILES #returns a dict-like object
+        doc_name = doc['image']
+        fs = FileSystemStorage()
+        filename = fs.save(doc_name.name, doc_name)
+        uploaded_file_url = fs.url(filename)
+        imagesToPreview.append(uploaded_file_url)
+        datajson = dumps(imagesToPreview)
+    return render(request , 'mainpage.html' , {'len':len(imagesToPreview),'imagesToPreview':imagesToPreview,'data':datajson})
 
 def bmi(request):
     return render(request , 'bmi.html' , {})
