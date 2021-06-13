fe9f6b0844241ebf234a545e219222b372e043ae
kmth-hash <bpk.san4ya@gmail.com>
2021-06-13 18:10:50 +0530

    Baby steps #1

--- myproj/myproj/settings.py
+++ myproj/myproj/settings.py
@@ -68,7 +68,7 @@ DATABASES = {
      'ENGINE': 'django.db.backends.postgresql_psycopg2',
      'NAME': 'I2R_DB',
      'USER': 'postgres',
-     'PASSWORD': 'dimi1234',
+     'PASSWORD': 'postgres',
      'HOST': 'localhost',
      'PORT': '',
      }
