91ecaa15bd65ecf62c76aff088f2f4e9108c96d2
DESKTOP-F9K9T9M\user <rajath749@gmail.com>
2021-07-05 11:28:55 +0530

    new models for database

--- myproj/myproj/settings.py
+++ myproj/myproj/settings.py
@@ -68,7 +68,7 @@ DATABASES = {
      'ENGINE': 'django.db.backends.postgresql_psycopg2',
      'NAME': 'I2R_DB',
      'USER': 'postgres',
-     'PASSWORD': '1234',
+     'PASSWORD': 'postgres',
      'HOST': 'localhost',
      'PORT': '',
      }
